993c3f29d24cc1884c028f01204aea2801948e79
nkolis <nurkholis010@gmail.com>
2023-01-25 22:20:16 +0700

    update index app

--- public/index.php
+++ public/index.php
@@ -2,10 +2,15 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
 use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\UserController;
 
 Router::add('GET', '/', HomeController::class, 'index', []);
+
+Router::add('GET', '/users/register', UserController::class, 'register', []);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
 
 
 Router::run();
