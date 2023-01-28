40aa30820cdd5e75122629be9c5332c6e119247e
nkolis <nurkholis010@gmail.com>
2023-01-28 16:04:46 +0700

    Add view dashboard

---
+++ app/View/Home/dashboard.php
@@ -0,0 +1,26 @@
+<?php
+
+use ProgrammerZamanNow\Belajar\PHP\MVC\Config\BaseURL;
+?>
+<div class="container col-xl-10 col-xxl-8 px-4 py-5">
+  <div class="row align-items-center g-lg-5 py-5">
+    <div class="col-lg-7 text-center text-lg-start">
+      <h1 class="display-4 fw-bold lh-1 mb-3">Selamat datang, <?= $model['user']['name'] ?></h1>
+      <p class="col-lg-10 fs-4">by <a target="_blank" href="https://www.programmerzamannow.com/">Programmer Zaman
+          Now</a></p>
+    </div>
+    <div class="col-md-10 mx-auto col-lg-5">
+      <div class="p-4 p-md-5 border rounded-3 bg-light">
+        <div class="form-floating mb-3">
+          <a href="<?= BaseURL::get() ?>/users/profile" class="w-100 btn btn-lg btn-primary">Profile</a>
+        </div>
+        <div class="form-floating mb-3">
+          <a href="<?= BaseURL::get() ?>/users/password" class="w-100 btn btn-lg btn-primary">Password</a>
+        </div>
+        <div class="form-floating mb-3">
+          <a href="<?= BaseURL::get() ?>/users/logout" class="w-100 btn btn-lg btn-danger">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
