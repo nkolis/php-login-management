85c3cfe6338f0c96d2337b3800d609b406d29a19
nkolis <nurkholis010@gmail.com>
2023-01-29 13:10:38 +0700

    Update index app

--- public/index.php
+++ public/index.php
@@ -17,5 +17,10 @@ Router::add('GET', '/users/login', UserController::class, 'login', [MustNotLogin
 Router::add('POST', '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
 Router::add('GET', '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
 
+Router::add('GET', '/users/profile', UserController::class, 'updateProfile', [MustLoginMiddleware::class]);
+Router::add('POST', '/users/profile', UserController::class, 'postUpdateProfile', [MustLoginMiddleware::class]);
+
+Router::add('GET', '/users/password', UserController::class, 'updatePassword', [MustLoginMiddleware::class]);
+Router::add('POST', '/users/password', UserController::class, 'postUpdatePassword', [MustLoginMiddleware::class]);
 
 Router::run();
