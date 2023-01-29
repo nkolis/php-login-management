d8a32faae43b9f6d92df91a22d83051d8793bba6
nkolis <nurkholis010@gmail.com>
2023-01-29 13:04:39 +0700

    Update user service

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -2,10 +2,11 @@
 
 namespace ProgrammerZamanNow\Belajar\PHP\MVC\Service;
 
+use Exception;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidateException;
-use ProgrammerZamanNow\Belajar\PHP\MVC\Model\{UserLoginRequest, UserLoginResponse, UserRegisterRequest, UserRegisterResponse};
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\{UserLoginRequest, UserLoginResponse, UserPasswordUpdateRequest, UserPasswordUpdateResponse, UserProfileUpdateRequest, UserProfileUpdateResponse, UserRegisterRequest, UserRegisterResponse};
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
 
 class UserService
@@ -76,6 +77,75 @@ class UserService
   {
     if ($request->id == null || $request->password == null || trim($request->id) == '' || trim($request->password) == '') {
       throw new ValidateException('id, password cannot blank');
+    }
+  }
+
+  public function updateProfileUser(UserProfileUpdateRequest $request): UserProfileUpdateResponse
+  {
+    $this->validateUserProfileUpdateRequest($request);
+
+    try {
+      Database::beginTransaction();
+      $user = $this->userRepository->findById($request->id);
+
+      if ($user == null) {
+        throw new ValidateException('User is not found');
+      }
+
+      $user->name = $request->name;
+
+      $this->userRepository->update($user);
+      Database::commitTransaction();
+      $response = new UserProfileUpdateResponse;
+      $response->user = $user;
+
+      return $response;
+    } catch (ValidateException $exception) {
+      Database::rollbackTransaction();
+      throw $exception;
+    }
+  }
+
+  private function validateUserProfileUpdateRequest(UserProfileUpdateRequest $request): void
+  {
+    if ($request->id == null || $request->name == null || trim($request->id) == '' || trim($request->name) == '') {
+      throw new ValidateException('id, name cannot blank');
+    }
+  }
+
+  public function updatePassword(UserPasswordUpdateRequest $request): UserPasswordUpdateResponse
+  {
+    $this->validateUserPasswordUpdateRequest($request);
+
+
+    try {
+      Database::beginTransaction();
+      $user = $this->userRepository->findById($request->id);
+      if ($user == null) {
+        throw new ValidateException('User not found');
+      }
+
+      if (!password_verify($request->oldPassword, $user->password)) {
+        throw new ValidateException('Old password is wrong');
+      }
+
+      $user->password = password_hash($request->newPassword, PASSWORD_BCRYPT);
+
+      $this->userRepository->update($user);
+      Database::commitTransaction();
+      $response = new UserPasswordUpdateResponse;
+      $response->user = $user;
+      return $response;
+    } catch (Exception $e) {
+      Database::rollbackTransaction();
+      throw $e;
+    }
+  }
+
+  private function validateUserPasswordUpdateRequest(UserPasswordUpdateRequest $request): void
+  {
+    if ($request->id == null || $request->oldPassword == null || $request->newPassword == null || trim($request->id) == '' || trim($request->oldPassword) == '' || trim($request->newPassword) == '') {
+      throw new ValidateException('id, old password, new password cannot blank');
     }
   }
 }
